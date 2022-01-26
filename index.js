410338ed9b42d726a05bfec55a5010c5214eb4cb
frankie0917 <frankie0917@foxmail.com>
2022-01-26 16:22:14 +0800

    OAuth

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import fs from 'fs';
+import readline from 'readline';
 import { google } from 'googleapis';
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json',
-  scopes: 'https://www.googleapis.com/auth/spreadsheets',
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_PATH = 'token.json';
+
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  authorize(JSON.parse(content), main);
 });
 
-const main = async () => {
-  const client = await auth.getClient();
-  const googleSheet = google.sheets({ version: 'v4', auth: client });
+const main = async (auth) => {
+  const googleSheet = google.sheets({ version: 'v4', auth });
 
-  const spreadsheetId = '1JVhKEAUt_-F2Hd71MUvUPQPz4dfb3pwPRe_O2cs1rQA';
+  const spreadsheetId = 'spreadsheetId';
   const res = await googleSheet.spreadsheets.get({
     auth,
     spreadsheetId,
@@ -19,4 +22,47 @@ const main = async () => {
   fs.writeFileSync('data.json', JSON.stringify(res.data, null, 2));
 };
 
-main();
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0],
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err)
+        return console.error(
+          'Error while trying to retrieve access token',
+          err,
+        );
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
